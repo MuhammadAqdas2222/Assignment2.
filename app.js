fc73ff88aed66397880b894667d1fd5562d8a4ae
MuhammadAqdus2222 <aqdasaslam2222@gmail.com>
2021-12-15 20:51:19 +0500

    assignment complete

---
+++ app.js
@@ -0,0 +1,49 @@
+var name = window. prompt("Enter your First name");
+var name = window. prompt("Enter your  Last name");
+var a=prompt("enter first name")
+var b=prompt("enter last name")
+var c= a+b
+console.log( "Hello"  +c)
+
+var mobile = prompt("Enter your favorite mobile phone name");
+var charsInmobile = mobile.length;
+console.log(charsInmobile)
+
+var x = ("Pakistani");
+ var xposition=x.indexOf("n");
+ console.log(xposition)
+
+var x = ("Hello World");
+ var xposition=x.lastIndexOf("l");
+ console.log(xposition);
+
+let text = "Pakistani";
+let letter = text.charAt(3);
+console.log(letter)
+
+let text = " I live in hyderabad city . hyderabad city is beautiful";
+let result = text.replace(/hyder/g, "islam");
+console.log(result)
+ 
+
+
+let text = "“Ali and Sami are best friends. They play cricket and football together ";
+let result = text.replace(/and/g, "&");
+console.log(result)
+
+
+
+ var text = "Muhammad";
+ var qualifyingAge = parseInt(text);
+ console.log(qualifyingAge);
+ console.log(typeof qualifyingAge);
+
+var capital=prompt("Enter your words");
+alert(capital.toUpperCase());
+
+var capital=prompt("Enter your words");
+alert(capital.toTitleCase());
+
+
+ var num= "35.36"
+ console.log(parseInt(num));
